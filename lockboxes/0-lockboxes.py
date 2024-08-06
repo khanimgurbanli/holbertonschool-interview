9e7e6cb1eee34dfc2e83f15cf803d82db3806593
khanimgurbanli <info@khanimgurbanli.com>
2024-08-06 19:51:13 +0400

    holbertonschool-interview

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -13,22 +13,17 @@ More informations:
 
 def canUnlockAll(boxes):
     n = len(boxes)  # Number of boxes
+    visited = [False] * n  # Keep track of visited boxes
+    visited[0] = True  # The first box is always open
+    queue = [0]  # Start with the first box in the queue
 
-    # To track which boxes have been visited
-    visited = [False] * n  #  mean is  visited = [False, False, False]
-    visited[0] = True  #  mean is  visited = [True, False, False]
-
-    # Queue for Breadth-First Search (BFS)
-    queue = [0]  # Start with the first box
-
-    # Breadth-First Search (BFS)
     while queue:
         current = queue.pop(0)  # Get the next box to process
+        for key in boxes[current]:  # Check all keys in the current box
+            if (
+                key < n and not visited[key]
+            ):  # If the key is valid and the box is not visited
+                visited[key] = True  # Mark the box as visited
+                queue.append(key)  # Add the box to the queue for further exploration
 
-        # Check each key in the current box
-        for box in boxes[current]:
-            if box < n and not visited[box]:  # Check if box index is valid
-                visited[box] = True  # Mark it as visited
-                queue.append(box)  # Add it to the queue for next exploration
-
-    return all(visited)
+    return all(visited)  # Return True if all boxes are visited, else False
--- lockboxes/main_0.py
+++ lockboxes/main_0.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 canUnlockAll = __import__("0-lockboxes").canUnlockAll
+# Example test cases
+boxes1 = [[1], [2], [3], [4], []]
+boxes2 = [[1, 2, 3], [0], [4], [2], [1, 5], [6], []]
+boxes3 = [[1, 3], [3, 0, 1], [2], [0]]
 
-# Test Cases
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))  # return False
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))  # return True
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))  # return False
+print(canUnlockAll(boxes1))  # Should return True
+print(canUnlockAll(boxes2))  # Should return False
+print(canUnlockAll(boxes3))  # Should return False
