eb1fa806b8bf2ce3ba6063c84e257cd2c003e51d
khanimgurbanli <info@khanimgurbanli.com>
2024-09-13 19:40:31 +0400

    fix

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Reads stdin line by line and computes metrics
 If count of lines is evenly divided by 10 and/or
-keyboard interrupt
+keyboardinterrupt
 all info will be printed"""
 
 import sys
